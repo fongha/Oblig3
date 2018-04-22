724613a0c25264e6ceda5ff82476d435d58539ab
fongha <35597502+fongha@users.noreply.github.com>
2018-04-22 15:20:53 +0200

    Update client.go

--- opg3/client.go
+++ opg3/client.go
@@ -10,10 +10,10 @@ import (
 //For å teste serverfunksjonalitet kan udp og tcp skrives inn avhengig av hvilken server du skal koble til.
 func main() {
 	fmt.Println("Vil du koble til serveren gjennom UDP eller TCP?")
-	nettverk := bufio.NewScanner(os.Stdin)
-	for nettverk.Scan() {
+	network := bufio.NewScanner(os.Stdin)
+	for network.Scan() {
 		p := make([]byte, 2048)
-		conn, err := net.Dial(nettverk.Text(), "127.0.0.1:17")
+		conn, err := net.Dial(network.Text(), "127.0.0.1:17")
 		if err != nil {
 			fmt.Printf("Some error %v", err)
 			return
